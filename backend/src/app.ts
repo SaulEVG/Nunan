0e748216228bd0e40a5a1d490b4a190b2ed45378
SaulEVG <saulevg.services@gmail.com>
2024-06-24 20:31:38 -0300

    adding a just a test on "/" endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,9 @@ const PORT = process.env.PORT ?? 8080;
 const app = express();
 
 app.use(cors());
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "Hola perro" });
+});
 app.use("/api", router);
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
